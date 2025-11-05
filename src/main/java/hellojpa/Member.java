fb18162e07ebc81fb15a93b483a20afd4ccabfa9
OhJunTaekk <mt3026super@naver.com>
2025-11-05 21:16:11 +0900

    ~2025.11.5

---
+++ src/main/java/hellojpa/Member.java
@@ -0,0 +1,27 @@
+package hellojpa;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class Member {
+    @Id
+    private Long id;
+    private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
